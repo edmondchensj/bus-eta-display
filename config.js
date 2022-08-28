df92e5baff10ca78d137854eb653d75141f0b68a
Edmond Chen <edmondchen@Edmonds-MacBook-Pro-2.local>
2022-08-28 15:06:16 +0800

    Add config for prod

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 window._config = {
     apiUrl: {
-        getETA: "https://9iurrllg68.execute-api.ap-southeast-1.amazonaws.com/get-bus-eta"
+        getETA: "https://gz40sccuk8.execute-api.ap-southeast-1.amazonaws.com/get-bus-eta"
     },
     env: {
         getETAUserName: "t4_bus_display_driver",
         getETAPassword: "871BP7vh*G%X"
     },
-    account: "SANDBOX"
+    account: "PROD (LZ)"
 };
